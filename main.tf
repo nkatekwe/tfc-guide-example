fd55db38f8be19d5d374591c8d059dfdb8a36e01
nkatekwe <55753494+nkatekwe@users.noreply.github.com>
2026-05-13 17:25:25 +0200

    Migrate from AWS to Azure resources in main.tf

--- main.tf
+++ main.tf
@@ -1,31 +1,57 @@
 # Copyright (c) HashiCorp, Inc.
 # SPDX-License-Identifier: MPL-2.0
 
-provider "aws" {
-  region = var.region
+provider "azurerm" {
+  features {}
+  # Optional: specify subscription_id if needed
+  # subscription_id = var.subscription_id
 }
 
-data "aws_ami" "ubuntu" {
-  most_recent = true
-
-  filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
-  }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-
-  owners = ["099720109477"] # Canonical
+data "azurerm_platform_image" "ubuntu" {
+  location  = var.location
+  publisher = "Canonical"
+  offer     = "0001-com-ubuntu-server-focal"
+  sku       = "20_04-lts"
 }
 
-resource "aws_instance" "ubuntu" {
-  ami           = data.aws_ami.ubuntu.id
-  instance_type = var.instance_type
-
+resource "azurerm_linux_virtual_machine" "ubuntu" {
+  name                = var.instance_name
+  resource_group_name = var.resource_group_name
+  location            = var.location
+  size                = var.instance_type
+  
+  admin_username = "azureuser"
+  
+  network_interface_ids = [
+    azurerm_network_interface.ubuntu.id
+  ]
+  
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+  
+  source_image_reference {
+    publisher = data.azurerm_platform_image.ubuntu.publisher
+    offer     = data.azurerm_platform_image.ubuntu.offer
+    sku       = data.azurerm_platform_image.ubuntu.sku
+    version   = data.azurerm_platform_image.ubuntu.version
+  }
+  
   tags = {
     Name = var.instance_name
+  }
+}
+
+# Required: Network interface for the VM
+resource "azurerm_network_interface" "ubuntu" {
+  name                = "${var.instance_name}-nic"
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = var.subnet_id
+    private_ip_address_allocation = "Dynamic"
   }
 }
