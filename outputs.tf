08bc0843f9924ddc3dafc0e2c6e2d13729b9db29
nkatekwe <55753494+nkatekwe@users.noreply.github.com>
2026-05-13 17:26:18 +0200

    Refactor outputs for Azure VM configuration
    Updated output variables for Azure virtual machine details.

--- outputs.tf
+++ outputs.tf
@@ -1,11 +1,24 @@
 # Copyright (c) HashiCorp, Inc.
 # SPDX-License-Identifier: MPL-2.0
 
-output "instance_ami" {
-  value = aws_instance.ubuntu.ami
+output "vm_image_id" {
+  value = azurerm_linux_virtual_machine.ubuntu.source_image_reference.version
 }
 
-output "instance_arn" {
-  value = aws_instance.ubuntu.arn
+output "vm_id" {
+  value = azurerm_linux_virtual_machine.ubuntu.id
 }
 
+# Additional useful Azure outputs:
+
+output "vm_private_ip_address" {
+  value = azurerm_linux_virtual_machine.ubuntu.private_ip_address
+}
+
+output "vm_public_ip_address" {
+  value = azurerm_linux_virtual_machine.ubuntu.public_ip_address
+}
+
+output "vm_resource_id" {
+  value = azurerm_linux_virtual_machine.ubuntu.virtual_machine_id
+}
