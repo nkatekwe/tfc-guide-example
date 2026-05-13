3a09e771ac6b1c0f7c092cc8a864397f8081f5f2
nkatekwe <55753494+nkatekwe@users.noreply.github.com>
2026-05-13 17:27:00 +0200

    Add Azure variables to variables.tf
    Added Azure-specific variables for location, instance type, instance name, resource group, and subnet ID.

--- variables.tf
+++ variables.tf
@@ -1,18 +1,29 @@
 # Copyright (c) HashiCorp, Inc.
 # SPDX-License-Identifier: MPL-2.0
 
-variable "region" {
-  description = "AWS region"
-  default     = "us-west-1"
+variable "location" {
+  description = "Azure region"
+  default     = "westus"
 }
 
 variable "instance_type" {
-  description = "Type of EC2 instance to provision"
-  default     = "t2.micro"
+  description = "Type of Azure VM to provision"
+  default     = "Standard_B1s"
 }
 
 variable "instance_name" {
-  description = "EC2 instance name"
+  description = "Azure VM name"
   default     = "Provisioned by Terraform"
 }
 
+# Additional required Azure variables (not in AWS original)
+
+variable "resource_group_name" {
+  description = "Name of the resource group"
+  default     = "terraform-example-rg"
+}
+
+variable "subnet_id" {
+  description = "ID of the subnet where the VM will be deployed"
+  default     = null
+}
